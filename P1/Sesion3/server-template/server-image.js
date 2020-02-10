137767c348e0e01b6fbdd2fc723ffda5ca4a0365
Alejandro Estaban Lopez <aesteban@f-l3208-pc14.aulas.etsit.urjc.es>
2020-02-10 13:07:59 +0100

    Servidor en pruebas(buscar nombre fichero)

---
+++ P1/Sesion3/server-template/server-image.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+const PUERTO = 8080
+
+//-- Configurar y lanzar el servidor. Por cada peticion recibida
+//-- se imprime un mensaje en la consola
+http.createServer((req, res) => {
+  console.log("----------> Peticion recibida")
+  let q = url.parse(req.url, true);
+
+  let filename = ""
+
+  //-- Obtener fichero a devolver
+
+  filename  = "." + q.pathname
+
+  console.log("Recurso:" + filename)
+  console.log("Terminacion:" + path.extname(q.pathname))
+
+
+  //-- Leer fichero
+  fs.readFile(filename, function(err, data) {
+
+    //-- Fichero no encontrado. Devolver mensaje de error
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+
+    //-- Tipo mime por defecto: html
+    let mime = "text/html"
+
+    //-- Generar el mensaje de respuesta
+    res.writeHead(200, {'Content-Type': mime});
+    res.write(data);
+    res.end();
+  });
+
+
+}).listen(PUERTO);
+
+console.log("Servidor corriendo...")
+console.log("Puerto: " + PUERTO)
