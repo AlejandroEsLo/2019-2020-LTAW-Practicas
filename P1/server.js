f458d16502b361f2e895bb4b2e81f55bf2770a01
AlejandroEsLo <alex-lojim@hotmail.com>
2020-02-11 18:36:29 +0100

    Servidor con node FINALIZADO(comprobado con mi pagina el funcionamiento)

--- P1/server.js
+++ P1/server.js
@@ -1,35 +1,64 @@
-//-- Puerto donde recibir las peticiones
-const PUERTO = 8080;
-
-//-- Modulo http
 const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+const PUERTO = 8080
 
-console.log("Arrancando servidor...")
+//-- Configurar y lanzar el servidor. Por cada peticion recibida
+//-- se imprime un mensaje en la consola
+http.createServer((req, res) => {
+  console.log("----------> Peticion recibida")
+  let q = url.parse(req.url, true);
 
-//-- Funcion para atender a una Peticion
-//-- req: Mensaje de solicitud y res: Mensaje de respuesta
-function peticion(req, res) {
+  //console.log("search: " + q.search)  // Buscar objeto
+  console.log("Recurso:" + q.pathname)
+  console.log("Terminacion:" + path.extname(q.pathname))
+  console.log("Nombre:" + path.basename(q.pathname))
 
-  //-- Peticion recibida
-  console.log("Peticion recibida!")
-  console.log("Recurso solicitado (URL): " + req.url)
-  console.log(req.headers)
+  //-- Creamos varaibles para obtener terminaciones y el objeto que queremos
+  let terminacion = path.extname(q.pathname)
+  let recurso = q.pathname
 
-  //-- Crear mensaje de respuesta
-  //res.writeHead(200, {'Content-Type': 'text/html'});
-  //res.end('Hello World!');
-}
+  //-- Obtener fichero a devolver(Para que coja al principio el index.html)
+  if (q.pathname == "/")
+    recurso = "index.html"
+  //--- OBTENER RECURSO ENTERO
+  recurso = "./" + recurso
 
-//var balon = new Image(600,400)
-//balon.src = './balon.jpg'
-//-- Inicializar el servidor
-//-- Cada vez que recibe una petición
-//-- invoca a la funcion peticion para atenderla
-const server = http.createServer(peticion)
+  //-- Leer fichero
+  fs.readFile(recurso, function(err, data) {
 
-//-- Configurar el servidor para escuchar en el
-//-- puerto establecido
-server.listen(PUERTO);
+    //-- Fichero no encontrado. Devolver mensaje de error
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
 
-console.log("Servidor LISTO!")
-console.log("Escuchando en puerto: " + PUERTO)
+    //-- Tipo mime por defecto: html
+    let mime = ""
+
+    if (terminacion == ".css"){
+      mime = "text/css"
+
+    }else if(terminacion == ".jpg"){
+      mime = "image/jpg"
+
+    }else if(terminacion == ".png"){
+      mime = "image/png"
+
+    }else {
+      mime = "text/html"
+    }
+
+    console.log("MIME:" + mime)
+    //-- Generar el mensaje de respuesta
+    res.writeHead(200, {'Content-Type': mime});
+    res.write(data);
+    res.end();
+  });
+
+
+}).listen(PUERTO);
+
+console.log("Servidor corriendo...")
+console.log("Puerto: " + PUERTO)
