eaa7fe8d780d6bb12b9d64ff7201b137a6047e68
Alejandro Estaban Lopez <aesteban@f-l3210-pc13.aulas.etsit.urjc.es>
2020-02-18 10:30:05 +0100

    Creando paginas dinamicas!!!

--- P2/mi_tienda_web/tienda_peces/views.py
+++ P2/mi_tienda_web/tienda_peces/views.py
@@ -10,3 +10,18 @@ def index(request):
     # -- Obtener el número aleatorio
     numero = randint(0, 100)
     return render(request, 'index.html', {'numero':str(numero)})
+
+def producto1(request):
+    # -- Obtener el número aleatorio
+    numero = randint(0, 100)
+    return render(request, 'producto1.html', {'numero':str(numero)})
+
+def producto2(request):
+    # -- Obtener el número aleatorio
+    numero = randint(0, 100)
+    return render(request, 'producto2.html', {'numero':str(numero)})
+
+def producto3(request):
+    # -- Obtener el número aleatorio
+    numero = randint(0, 100)
+    return render(request, 'producto3.html', {'numero':str(numero)})
