05ae768d20a3a9e09cefb81db1ef5b0dbfbb5849
Alejandro Estaban Lopez <aesteban@f-l3208-pc14.aulas.etsit.urjc.es>
2020-02-24 12:56:36 +0100

    1)Visualizacion de listado.html y creacion de css para modificar el estilo. 2)Modificacion de settings para verlo desde otro ordenador.

--- P2/mi_tienda_web/mi_tienda_web/settings.py
+++ P2/mi_tienda_web/mi_tienda_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q-(dx!76n^r7l%++t@#b+wha_bo9l_17#f-x+2@n-mu%z7$3@m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
