ad8d5ec9696b453e017503d6f0c78e8cb5eadb31
Alejandro Estaban Lopez <aesteban@f-l3208-pc14.aulas.etsit.urjc.es>
2020-03-09 13:06:55 +0100

    probando formularios y login

---
+++ P3/prueba.js
@@ -0,0 +1,106 @@
+const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+const PUERTO = 8080
+
+
+//-- Funcion para atender a una Peticion
+//-- req: Mensaje de solicitud
+//-- res: Mensaje de respuesta
+function peticion(req, res) {
+
+  //-- Mostrar en la consola el recurso al que se accede
+  const q = url.parse(req.url, true);
+  console.log("Petición: " + q.pathname)
+
+  //-- Creamos varaibles para obtener terminaciones y el objeto que queremos
+  let terminacion = path.extname(q.pathname)
+  let recurso = q.pathname
+
+  //-- Leer las cookies
+  const cookie = req.headers.cookie;
+  console.log("Cookie: " + cookie)
+
+  //-- Segun el recurso al que se accede
+  switch (q.pathname) {
+
+    //-- Pagina principal
+    case "/":
+      recurso = "index.html"
+      //--- OBTENER RECURSO ENTERO
+      recurso = "./" + recurso
+
+      //-- No hay ninguna cookie
+      if (!cookie) {
+        content = "\nNo te conozco... Registrate!\n"
+        content = "Accede a /login"
+
+      //-- Hay definida una Cookie.
+      } else {
+        content = "Cookie encontrada"
+      }
+
+      res.statusCode = 200;
+      break;
+
+    //-- Pagina de acceso
+    case "/login":
+      content = "Registrado! Cookie enviada al navegador!"
+      recurso = "index.html"
+      //--- OBTENER RECURSO ENTERO
+      recurso = "./" + recurso
+
+      //-- ESTABLECER LA COOKIE!! En el campo set-cookie metemos la cookie que tengamos
+      res.setHeader('Set-Cookie', 'user=Cookie encontrada')
+      break
+
+    //-- Se intenta acceder a cualquier otro recurso
+    default:
+      recurso = q.pathname
+      recurso = "./" + recurso
+  }
+
+  //-- Leer fichero
+  fs.readFile(recurso, function(err, data) {
+
+    //-- Fichero no encontrado. Devolver mensaje de error
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+
+    //-- Tipo mime por defecto: html
+    let mime = ""
+
+    if (terminacion == ".css"){
+      mime = "text/css"
+
+    }else if(terminacion == ".jpg"){
+      mime = "image/jpg"
+
+    }else if(terminacion == ".png"){
+      mime = "image/png"
+
+    }else {
+      mime = "text/html"
+    }
+  //-- Generar el mensaje de respuesta
+  res.setHeader('Content-Type', mime)
+  res.write(data);
+  res.end();
+  });
+console.log(content)
+}
+
+//-- Inicializar el servidor
+//-- Cada vez que recibe una petición
+//-- invoca a la funcion peticion para atenderla
+const server = http.createServer(peticion)
+
+//-- Configurar el servidor para escuchar en el
+//-- puerto establecido
+server.listen(PUERTO);
+
+console.log("Servidor LISTO!")
+console.log("Escuchando en puerto: " + PUERTO)
---
+++ P3/server.js
@@ -0,0 +1,69 @@
+const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+const PUERTO = 8080
+
+
+//-- Configurar y lanzar el servidor. Por cada peticion recibida
+//-- se imprime un mensaje en la consola
+http.createServer((req, res) => {
+  console.log("----------> Peticion recibida")
+  let q = url.parse(req.url, true);
+
+  //console.log("search: " + q.search)  // Buscar objeto
+  console.log("Recurso:" + q.pathname)
+  console.log("Terminacion:" + path.extname(q.pathname))
+  console.log("Nombre:" + path.basename(q.pathname))
+
+  //-- Creamos varaibles para obtener terminaciones y el objeto que queremos
+  let terminacion = path.extname(q.pathname)
+  let recurso = q.pathname
+
+  //-- Leer las cookies
+  const cookie = req.headers.cookie;
+  console.log("Cookie: " + cookie)
+
+  //-- Obtener fichero a devolver(Para que coja al principio el index.html)
+  if (q.pathname == "/")
+    recurso = "index.html"
+  //--- OBTENER RECURSO ENTERO
+  recurso = "./" + recurso
+
+  //-- Leer fichero
+  fs.readFile(recurso, function(err, data) {
+
+    //-- Fichero no encontrado. Devolver mensaje de error
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+
+    //-- Tipo mime por defecto: html
+    let mime = ""
+
+    if (terminacion == ".css"){
+      mime = "text/css"
+
+    }else if(terminacion == ".jpg"){
+      mime = "image/jpg"
+
+    }else if(terminacion == ".png"){
+      mime = "image/png"
+
+    }else {
+      mime = "text/html"
+    }
+
+  console.log("MIME:" + mime)
+  //-- Generar el mensaje de respuesta
+  res.writeHead(200, {'Content-Type': mime});
+  res.write(data);
+  res.end();
+  });
+
+
+}).listen(PUERTO);
+
+console.log("Servidor corriendo...")
+console.log("Puerto: " + PUERTO)
